e8b6666379d72099c79d17d2ab0c21f2df6df254
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-16 13:32:21 +0300

    Exercise 1

---
+++ EX_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число = ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (n / 1000 == (n % 10) * 10 + (n % 100) / 10)
+        {
+            Console.WriteLine("Является палиндромом: Да ");
+        }
+
+        else
+        {
+            Console.WriteLine("Является палиндромом: Нет ");
+        }
+    }
+
+}

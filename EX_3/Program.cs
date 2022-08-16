fc69d4d506bb4baa3940cfc2512b7dd71ff41287
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-16 13:35:27 +0300

    Exercise 3

---
+++ EX_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите кол-во чисел для вывода: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= N; i++)
+            Console.WriteLine(i + " => " + i * i * i);
+    }
+
+
+}
